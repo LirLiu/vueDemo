3cdf752f7636c84c11391238f8b48db162d67f32
LirLiu <1720564643@qq.com>
2020-06-04 16:15:13 +0800

    #0008#导航栏重构

--- src/components/GlobalHeader/GlobalHead.vue
+++ src/components/GlobalHeader/GlobalHead.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="headWrapper">
-    <div>
-      <div>导航栏图标</div>
-      <div>
-        <ul>
-          <li>首页</li>
-          <li>分享帖</li>
-          <li>游记</li>
-          <li>约伴</li>
-          <li>问答</li>
-        </ul>
-      </div>
+    <div class="hwLeft">
+      <ul>
+        <li>
+          <router-link to="/">
+            <i class="el-icon-soccer"></i>主站
+          </router-link>
+        </li>
+      </ul>
     </div>
-    <div>
-      <div>{{msg}}搜索栏</div>
-      <div>登录</div>
+    <div class="hwCenter">
+      <img src="../../assets/image/resizeApi.png" alt="晴雨视频" />
+    </div>
+    <div class="hwRight">
+      <div>
+        <img src="../../assets/image/akari.jpg" alt="头像" />
+      </div>
+      <div v-if="loginVersion">
+        <router-link to="/">个人中心</router-link>
+        <router-link to="/">动态</router-link>
+        <router-link to="/">会员</router-link>
+      </div>
+      <div v-else>
+        <router-link to="/">登录</router-link>
+        <router-link to="/">注册</router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -23,6 +33,7 @@ export default {
   name: "GlobalHead",
   data() {
     return {
+      loginVersion: false,
       msg: "hello world"
     };
   }
