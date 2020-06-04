ab894498c5b280833035b109d38b4116a8d6c16e
LirLiu <1720564643@qq.com>
2020-06-04 13:28:58 +0800

    #0007#首页添加轮播图

--- src/components/Home/GlobalHome.vue
+++ src/components/Home/GlobalHome.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="homeWrapper">
+    <home-swiper></home-swiper>
     <div>轮播图</div>
     <div>轮播图</div>
     <div>轮播图</div>
   </div>
 </template>
+<script>
+import HomeSwiper from "./HomeSwiper";
+
+export default {
+  name: "GlobalHome",
+  components: {
+    HomeSwiper
+  }
+};
+</script>
 <style lang='less' scoped>
 .homeWrapper {
   > div {
